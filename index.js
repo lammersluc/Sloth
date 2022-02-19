2d90e30f37c55aad9a4584be4395d41aed530b66
NotTrackz <luclammers06@gmail.com>
2022-02-19 21:32:33 +0100

    test

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 require('./addons')
+const express = require('express')
+const app = express()
+const port = 80
 const Discord = require('discord.js')
 const { Client, Intents } = require('discord.js')
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, , Intents.FLAGS.GUILD_MESSAGES] })
@@ -27,5 +30,7 @@ client.on('messageCreate', message => {
   require('./event/messageCreate') (client, message)
 })
 
-
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
 client.login('OTAwMTA4NzM5MzcyMjY1NTEy.YW8hlg.aIJc46zAiRJo0D4FlW7W2Pbuzxc')
