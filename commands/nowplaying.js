2b5a1495619fcf8cf214aee9987f933aa3f1be32
NotTrackz <luclammers06@gmail.com>
2022-04-03 19:40:32 +0200

    fixed now playing command error

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -12,7 +12,7 @@ module.exports = {
     run: async (client, message, args) => {
         const queue = client.distube.getQueue(message)
         if (!queue) return message.channel.send(`There is nothing playing!`)
-        const song = queue.song
+        const song = queue.songs[0]
         message.channel.send(
             `Now playing: \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${
               song.user}`
