e6c3bc6ebd0adafafc964fc4aba4b7554937e523
NotTrackz <luclammers06@gmail.com>
2022-04-03 19:34:38 +0200

    added now playing command

---
+++ commands/nowplaying.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: 'nowplaying',
+    helpname: 'Now Playing',
+    aliases: ['np'],
+    aliasesText: 'NP',
+    description: 'Shows info about the current song that is playing.',
+    usage: 'NowPlaying',
+    enabled: true,
+    visible: true,
+    devOnly: false,
+    servAdmin: false,
+    run: async (client, message, args) => {
+        const queue = client.distube.getQueue(message)
+        if (!queue) return message.channel.send(`There is nothing playing!`)
+        queue.textChannel.send(
+            `Now playing: \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${
+              song.user}`
+          )
+    }
+}
