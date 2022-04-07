b1acda29d09e0246827416ebcbccfe9697939732
Not Trackz <86626234+NotTrackz@users.noreply.github.com>
2022-04-07 12:16:35 +0200

    removed ! from now playing command nothing playing

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -11,7 +11,7 @@ module.exports = {
     servAdmin: false,
     run: async (client, message, args) => {
         const queue = client.distube.getQueue(message)
-        if (!queue) return message.channel.send(`There is nothing playing!`)
+        if (!queue) return message.channel.send(`There is nothing playing.`)
         const song = queue.songs[0]
         message.channel.send(
             `Now playing: \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${
