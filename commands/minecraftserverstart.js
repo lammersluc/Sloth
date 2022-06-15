73df440bd89895678132d3cfb856b63d8519f7a8
Not Trackz <luclammers06@gmail.com>
2022-06-15 21:54:57 +0200

    readded minecraftserverstart

---
+++ commands/minecraftserverstart.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require('discord.js')
+const { tcpPingPort } = require('tcp-ping-port')
+const pm2 = require('pm2')
+
+module.exports = {
+    name: 'minecraftserverstart',
+    helpname: 'MC Server Start',
+    aliases: ['mcss'],
+    aliasesText: ' ',
+    description: 'Starts the mc server.',
+    usage: 'mcserverstart',
+    enabled: true,
+    visible: false,
+    devOnly: false,
+    servAdmin: false,
+    run: async (client, message, args) => {
+        let embed = new MessageEmbed().setColor(client.embedColor)
+        const ping = await tcpPingPort('mc.nottrackz.me', 25565)
+        if (ping.online) return message.channel.send({ embeds: [embed.setDescription('The server is already online.')] })
+        message.channel.send({ embeds: [embed.setDescription('Starting server...')] })
+        pm2.restart('MCServer')
+    }
+} 
