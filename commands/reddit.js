db0448562cd94edb155595e70fb6cc5bdcfad214
lammersluc <luclammers06@gmail.com>
2022-11-04 20:28:51 +0100

    fixed reddit command

--- commands/reddit.js
+++ commands/reddit.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const { EmbedBuilder } = require('discord.js');
+const { EmbedBuilder, Embed } = require('discord.js');
 const moment = require('moment');
 
 module.exports = {
@@ -14,15 +14,15 @@ module.exports = {
     devOnly: false,
     servAdmin: false,
     run: async (client, message, args) => {
+        let embed = new EmbedBuilder();
         if(!args[0]) {
-            return message.channel.send({ embeds: [embed.setColor(client.embedColor).setDescription(`Supply a Subreddit`)] });
+            return message.channel.send({ embeds: [embed.setColor(client.embedColor).setDescription(`Supply a Subreddit.`)] });
         }
         try {
             let data = await axios.get(`https://www.reddit.com/r/${args[0]}/random/.json`);
-            let embed = new EmbedBuilder()
+            embed
                 .setAuthor({name:`${data.data[0].data.children[0].data.subreddit_name_prefixed} | ${moment(Number(data.data[0].data.children[0].data.created * 1000)).format('Do MMMM YYYY, h:mm a')}`})
                 .setTitle(`${data.data[0].data.children[0].data.title}`)
-                .setDescription(`${data.data[0].data.children[0].data.selftext}`)
                 .setFooter({text:`u/${data.data[0].data.children[0].data.author} | ${data.data[0].data.children[0].data.ups} Upvotes | ${data.data[0].data.children[0].data.num_comments} Comment(s)`})
                 .setURL(`https://www.reddit.com${data.data[0].data.children[0].data.permalink}`)
                 .setColor(client.embedColor);
@@ -38,7 +38,7 @@ module.exports = {
             }
 
         } catch(e) {
-            message.channel.send({ embeds: [embed.setColor(client.embedColor).setDescription('Subreddit doesn\'t exist')] });
+            message.channel.send({ embeds: [embed.setColor(client.embedColor).setDescription('Subreddit doesn\'t exist.')] });
         }
     }
 }
