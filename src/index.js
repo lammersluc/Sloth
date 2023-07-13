00f0908b9892737d7e1f0a4da49e475d8e845de9
Luc Lammers <luclammers06@gmail.com>
2023-07-13 12:43:57 +0200

    - restriction error send

--- src/index.js
+++ src/index.js
@@ -12,45 +12,47 @@ client.musicquiz = [];
 
 process.on('uncaughtException', (e) => {
 
-  client.devs.forEach(dev => {
+    if (e.stack.includes('Sign in to confirm your age')) return;
 
-    client.users.cache.get(dev).send({ embeds: [new EmbedBuilder().setTitle('Error').setDescription(`\`\`\`${e.stack}\`\`\``).setColor(client.embedColor)] });
+    client.devs.forEach(dev => {
 
-  });
+        client.users.cache.get(dev).send({ embeds: [new EmbedBuilder().setTitle('Error').setDescription(`\`\`\`${e.stack}\`\`\``).setColor(client.embedColor)] });
+
+    });
 
 });
 
 setInterval(() => { client.user.setPresence({ activities: [{ name: `/Help | ${client.guilds.cache.size} Guilds`, type: ActivityType.Listening }], status: 'online' }); }, 60 * 60000);
 
 client
-  .on('ready', async () => {
+    .on('ready', async () => {
 
-    commandLoader(client);
+        commandLoader(client);
 
-    client.user.setPresence({ activities: [{ name: `/Help | ${client.guilds.cache.size} Guilds`, type: ActivityType.Listening }], status: 'online' });
-    console.log(`Logged in as ${client.user.tag}.`);
+        client.user.setPresence({ activities: [{ name: `/Help | ${client.guilds.cache.size} Guilds`, type: ActivityType.Listening }], status: 'online' });
+        console.log(`Logged in as ${client.user.tag}.`);
 
-  })
+    })
 
-  .on('interactionCreate', interaction => { return require('./events/interactionCreate.js') (client, interaction); })
+    .on('interactionCreate', interaction => { return require('./events/interactionCreate.js') (client, interaction); })
 
-  .on('messageCreate', message => {
+    .on('messageCreate', message => {
 
-    if (message.channel.parentId === '984118604805050398') return require('./events/ticketMessageCreate.js') (client, message);
-    if (message.channel.type === 1) return require('./events/dmMessageCreate.js') (client, message);
+        if (message.channel.parentId === '984118604805050398') return require('./events/ticketMessageCreate.js') (client, message);
+        if (message.channel.type === 1) return require('./events/dmMessageCreate.js') (client, message);
 
-  })
+    })
 
-  .on('channelDelete', channel => {
+    .on('channelDelete', channel => {
 
-    if (channel.parentId === '984118604805050398') require('./events/ticketClose.js') (client, channel);
+        if (channel.parentId === '984118604805050398') require('./events/ticketClose.js') (client, channel);
 
-  })
+    })
 
-  .on('guildDelete', () => {
+    .on('guildDelete', () => {
 
-    client.user.setPresence({ activities: [{ name: `${client.prefix}Help | ${client.guilds.cache.size} Guilds` }], status: 'online' });
+        client.user.setPresence({ activities: [{ name: `${client.prefix}Help | ${client.guilds.cache.size} Guilds` }], status: 'online' });
 
-  })
+    })
 
 client.login(process.env.DISCORD_TOKEN);
